63ff4365482b4e4220ceb248c9bce7e06e53cbf2
PrethigahShanmugarajah <prethigah@gmail.com>
2025-07-13 14:30:24 +0530

    Initial commit of Quiz Master Flutter app

---
+++ lib/quiz.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import 'package:quiz_master/start_screen.dart';
+import 'package:quiz_master/questions_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget activeScreen = const StartScreen();
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                const Color.fromARGB(255, 156, 39, 176),
+                const Color.fromARGB(255, 233, 30, 99),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
